896b532590934f08c272eb4db4d54b50980866b8
neopentane <shreeramkelkar7@gmail.com>
2019-09-28 10:08:48 +0530

    New?

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Candidate = require('./models/candidate');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 var cors = require('cors')
 const app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const startRoute=require('./routes/start');
 const questRoute=require('./routes/question');
 const adminRoute=require('./routes/admin');
-
+//const resultRoute=require('./routes/result');
 //Middlewere
 app.use(cors());
 app.use('/start',startRoute);
+//app.use('/result',resultRoute);
 app.use(bodyParser.json());
 app.use('/question',questRoute);
 app.use('/admin',adminRoute);
 app.use('/uploads', express.static('uploads'));
+
+
 
 //DataBase Connection
 mongoose.connect('mongodb://127.0.0.1/exam', {
@@ -54,4 +62,10 @@ app.post('/', async (req,res)=>{
     res.json({message : err});
   }
 });
+
+app.post('/result',async (req,res)=>{
+  var t= req.body;
+  console.log(t.answer);
+});
+
 app.listen(8081);
--- client/src/components/ExamHall.vue
+++ client/src/components/ExamHall.vue
@@ -5,26 +5,31 @@
       <div class="col-sm-8">
         <ul>{{current}}
           <div v-for="(i,index) in array" v-bind:key="index" v-if="current==index">
-            {{i.img}}
-            <img src="http://localhost:8081/uploads/1733c305a21bd3b38985732af7b1b62d" height=120px>
-            <li> {{i.question}} </li>
-              <ol> {{index}}
-                <div v-for="(j,index) in i.choices" v-bind:key="index">
-                  <input type="radio" value="asa" v-model="answer"> <li> {{j.option}} </li>
-                </div>
-              </ol>
-            </div>
-          </ul>
-        </div>
+            <img v-bind:src="url+i.img" height=120px>
+              <li> {{i.question}} </li>
+                <ol> {{index}}
+                  <div v-for="(j,index) in i.choices" v-bind:key="index">
+                    <input type="checkbox" v-model="answer" :value="j._id"> <li> {{j.option}} {{j.iscorrect}}</li>
+                  </div>
+                </ol>
+                <button v-on:click="current--" v-if="current!=0">Prev</button>
+                <button v-on:click="current++" v-if="current!=array.length">Save and Next</button>
+
+          </div>
+        </ul>
+      </div>
       <div class="col-sm-3">
         <div class="row">
-          <div class="col-sm-1" v-for="(i,a) in array" v-bind:key="i.id">
-            <button type="submit" style="padding: 8px 4px;" v-on:click="getcurrent(a)">{{a}}</button>
+          <div class="col-sm-3" v-for="(i,a) in array" v-bind:key="i.id">
+            <button type="submit" class="btn btn-primary" style="border-radius: 66%; padding:12px; background-color: #4CAF50;" v-on:click="getcurrent(a)">{{a+1}}</button>
           </div>
         </div>
       </div>
     </div>
-    <h1> {{current}} </h1>
+    <button
+        class="btn btn-primary" type="submit" @click="onSubmit">Submit
+    </button>
+    <h1> {{answer}} {{test}}</h1>
   </div>
 </template>
 
@@ -34,9 +39,12 @@ export default {
   name: 'ExamHall',
   data () {
     return {
+      url: 'http://localhost:8081/',
       array: null,
       current: 0,
-      image: null
+      image: null,
+      answer: [],
+      test: ''
     }
   },
   async mounted () {
@@ -47,6 +55,15 @@ export default {
   methods: {
     getcurrent: function (a) {
       this.current = a
+    },
+    onSubmit () {
+      console.log('hsere')
+      this.test = 'asdasd'
+      const formAnswer = {
+        answer: this.answer
+      }
+      console.log(formAnswer)
+      axios.post('http://localhost:8081/result/', formAnswer).then().catch(err => console.log(err))
     }
   }
 }
